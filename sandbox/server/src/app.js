04c41e9c39078dfdc5f8c196df7291aeff9fe4a6
Kanhaiya Arora <kanhaiyaarora75@gmail.com>
2026-08-04 00:26:47 +0530

    template is ready for our pod and now our server auto created pods by own. i create an api /api/sandbox/start for that and it return preview url.

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import { createPod } from "./kubernetes/pod.js";
+import { createService } from "./kubernetes/service.js";
+import { v7 as uuid } from "uuid";
 
 const app = express();
 
@@ -10,8 +13,27 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
 app.get("/api/sandbox/health", (req, res) => {
   res.status(200).json({ message: "Sandbox Api is running", status: "ok" });
+});
+
+app.post("/api/sandbox/start", async (req, res) => {
+  const sandboxId = uuid();
+  if (!sandboxId) {
+    return res.status(400).json({ message: "sandboxId is required" });
+  }
+  try {
+    await Promise.all([createPod(sandboxId), createService(sandboxId)]);
+    return res.status(201).json({
+      message: "Sandbox environment created successfully",
+      sandboxId,
+      previewUrl: `http://${sandboxId}.preview.localhost`,
+    });
+  } catch (error) {
+    console.error("Error creating sandbox:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 export default app;
