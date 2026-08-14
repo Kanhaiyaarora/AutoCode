f1057f5f30596ebc5c5f361ac29b873fc688eca9
Kanhaiya Arora <kanhaiyaarora75@gmail.com>
2026-08-15 01:52:19 +0530

    feat(k8s): implement deleteService and deletePod functions for sandbox resource cleanup on key expiration

--- sandbox/server/src/config/redis.js
+++ sandbox/server/src/config/redis.js
@@ -1,4 +1,6 @@
 import Redis from "ioredis";
+import { deleteService } from "../kubernetes/service.js";
+import { deletePod } from "../kubernetes/pod.js";
 
 const redis = new Redis(process.env.REDIS_URI);
 
@@ -19,10 +21,12 @@ subscriber.config("SET", "notify-keyspace-events", "Ex");
 
 subscriber.subscribe("__keyevent@0__:expired");
 
-subscriber.on("message", (channel, key) => {
+subscriber.on("message", async (channel, key) => {
   console.log(`key expired: ${key}`);
 
   const sandboxId = key.split(":")[1];
+  await deleteService(sandboxId);
+  await deletePod(sandboxId);
 });
 
 export default { subscriber };
